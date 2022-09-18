c47414b01294a6f3ae6f1d62ac066af168960e4f
Nikita Yankov <evne.yankovnikita@gmail.com>
2022-09-18 22:37:13 +0300

    Initial Commit

---
+++ 002_GridMagnification/_02_GridMagnificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  _02_GridMagnificationApp.swift
+//  002_GridMagnification
+//
+//  Created by nikita on 18.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct _02_GridMagnificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
